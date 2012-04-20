2a8e079c19be92d07ae4e9f38dc6245367741155
bm <bm@berkeley.edu>
2012-04-20 11:13:10 -0700

    scripts cleaned up

---
+++ single.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+MAIN_N=SPCEX
+
+# Make results folder if nonexistent, and check that it's empty
+mkdir -p results
+if [ "$(ls -A results)" ]; then
+    echo "There are other files in results/, possibly from older simulation runs.  Please remove them or move them out and rerun script."
+    exit 2
+fi
+
+# Build
+make clean && make o3
+qsub -v SIM_NAME=$MAIN_N -N $MAIN_N qsub.sh
+
+# Wait for simulation to finish running on the grid
+while [ $(ls results/*.jobdone | wc -l) -lt "1" ]; do
+    sleep 10
+done
+
+# Plot output RDF on gnuplot
+cd results && cat << EOF | gnuplot
+set term gif large size 1600, 1200
+set output "OO-RDF.gif"
+plot "$MAIN_N.out" using 1:2 with lp title 'O-O RDF'
+set output "AO-RDF.gif"
+plot "$MAIN_N.out" using 1:3 with lp title 'A-O RDF'
+set output "CO-RDF.gif"
+plot "$MAIN_N.out" using 1:4 with lp title 'C-O RDF'
+set output "AC-RDF.gif"
+plot "$MAIN_N.out" using 1:5 with lp title 'A-C RDF'
+set output "ALL-RDF.gif"
+plot "$MAIN_N.out" using 1:2 with lp title 'O-O RDF', "$MAIN_N.out" using 1:3 with lp title 'A-O RDF', "$MAIN_N.out" using 1:4 with lp title 'C-O RDF', "$MAIN_N.out" using 1:5 with lp title 'A-C RDF'
+EOF
+
+exit 0
